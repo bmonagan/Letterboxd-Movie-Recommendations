2c625726ff43995c245d9b7c87b5d04747207543
bmonagan <bmonagan@gmail.com>
2025-06-22 10:08:50 -0400

    Working on adding in a letterboxd option where it will scrape the user's watched films from their Letterboxd profile and return a list of those films. Will then offer recommendations based on the user's watched films.

---
+++ Testing/letterboxd_testing.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Fetch the webpage
+
+user_name = "breeban"
+url = f"https://letterboxd.com/{user_name}/films/diary/"
+response = requests.get(url)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extract specific data (e.g., all links)
+links = [a['href'] for a in soup.find_all('a', href=True)]
+soup.prettify()  # Format the HTML for better readability
+
+lb_watched = []
+for a in soup.find_all('a', href=True):
+    href = a['href']
+    if href.startswith(f'/{user_name}/film/'):
+        href = href.split('/')[3]  # Extract the film ID from the URL
+        href = href.replace("-", " ")
+        lb_watched.append(href)
+
+
+print(lb_watched)
